d2f85375e324221d9e40339ff9b362babde302ad
tjsals5559 <tjsals5559@gmail.com>
2026-05-16 14:24:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// --- 🤫 선생님만의 비밀 무기 (몰래 자리 배정) ---
+// true로 설정하면 버튼을 눌렀을 때 아래의 'secretSeats' 명단대로 배치됩니다.
+// 평소처럼 진짜 랜덤으로 섞고 싶다면 false로 바꿔주시면 됩니다!
+const useSecretMode = true; 
+
+// 미리 설정해둘 자리 배치도 (1번 자리부터 차례대로 이름을 적어주세요)
+// 특정 자리를 비워두려면 "" (큰따옴표 두 개)로 남겨두시면 됩니다.
+const secretSeats = [
+    "강백호", "서태웅", "송태섭", "정대만", "채치수", // 1~5번 자리
+    "권준호", "이달재", "신현철", "정우성", "이명헌", // 6~10번 자리
+    "학생11", "학생12", "학생13", "학생14", "학생15", // 11~15번 자리
+    "학생16", "학생17", "학생18", "학생19", "학생20", // 16~20번 자리
+    "학생21", "학생22", "학생23", "학생24", "학생25", // 21~25번 자리
+    "학생26", "학생27", "학생28", "학생29", "학생30", // 26~30번 자리
+    "학생31", "학생32", "학생33", "학생34", "학생35", // 31~35번 자리
+    "" // 36번 자리 (짝꿍 배열일 때 보통 남는 빈자리)
+];
+// ----------------------------------------------
+
+
 let students = Array.from({ length: 35 }, (_, i) => `학생 ${i + 1}`);
 let lockedSeats = {}; 
 
@@ -63,6 +83,27 @@ function shuffle() {
     const layout = layoutSelect.value;
     const totalSeats = layout === 'single' ? 35 : 36;
 
+    let newStudents = new Array(totalSeats).fill("");
+
+    // 🎯 비밀 모드가 켜져 있을 때의 로직
+    if (useSecretMode) {
+        for (let i = 0; i < totalSeats; i++) {
+            newStudents[i] = secretSeats[i] || "";
+        }
+        
+        // 비밀 모드라도, 선생님이 클릭해서 '고정'해둔 자리가 있다면 그 자리는 유지시켜 줍니다.
+        for (let i = 0; i < totalSeats; i++) {
+            if (lockedSeats[i] !== undefined) {
+                newStudents[i] = lockedSeats[i];
+            }
+        }
+        
+        students = newStudents;
+        renderSeats();
+        return; // 여기서 함수를 종료하여 진짜 랜덤 섞기를 건너뜁니다!
+    }
+
+    // 🎲 비밀 모드가 꺼져 있을 때의 진짜 랜덤 로직
     const rawText = studentNamesInput.value;
     let inputNames = rawText.split(/[,;\n\s]+/).map(s => s.trim()).filter(s => s.length > 0);
     
@@ -73,8 +114,6 @@ function shuffle() {
         const j = Math.floor(Math.random() * (i + 1));
         [flexibleNames[i], flexibleNames[j]] = [flexibleNames[j], flexibleNames[i]];
     }
-
-    let newStudents = new Array(totalSeats).fill("");
     
     for (let i = 0; i < totalSeats; i++) {
         if (lockedSeats[i] !== undefined) {
